d2bc03392c01585c0c6a72f22bb8052324d63d90
KashF <Kashfarhadi@gmail.com>
2019-06-18 23:59:21 -0400

    testing if this shows on github calendar as a change

--- snake.py
+++ snake.py
@@ -73,6 +73,7 @@ while key != 27:                                                   # While Esc k
         win.addch(last[0], last[1], ' ')
     win.addch(snake[0][0], snake[0][1], '#')
     
+    
 curses.endwin()
 print("\nScore - " + str(score))
 print("http://bitemelater.in\n")
